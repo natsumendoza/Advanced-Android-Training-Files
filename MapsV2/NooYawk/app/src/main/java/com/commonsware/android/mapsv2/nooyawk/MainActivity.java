60c533c509976552cdbd0bee5d110e7ffa5efd1b
natsumendoza <mendozalaxus@yahoo.com>
2017-04-26 17:01:45 +0800

    updates

--- MapsV2/NooYawk/app/src/main/java/com/commonsware/android/mapsv2/nooyawk/MainActivity.java
+++ MapsV2/NooYawk/app/src/main/java/com/commonsware/android/mapsv2/nooyawk/MainActivity.java
@@ -14,7 +14,10 @@
 
 package com.commonsware.android.mapsv2.nooyawk;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,6 +30,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
 
 public class MainActivity extends AbstractMapActivity implements
     OnMapReadyCallback, AdapterView.OnItemSelectedListener {
@@ -85,17 +90,38 @@ public class MainActivity extends AbstractMapActivity implements
   public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
     if (spinner.getSelectedItem().toString().equals("Fluxion Inc.")) {
+      Log.d(getClass().getSimpleName(), "You selected " + spinner.getSelectedItem().toString());
+
+      Geocoder geocoder = new Geocoder(this);
+      List<Address> addresses = null;
+
+      try {
+        addresses = geocoder.getFromLocationName("F1 Hotel Manila", 5);
+
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      if (addresses != null) {
+        for (Address address : addresses) {
+          Log.d(getClass().getSimpleName(), String.format("addresses long:lat %s:%s", address.getLatitude(), address.getLongitude()));
+        }
+      } else {
+        Log.d(getClass().getSimpleName(), "addresses is empty.");
+      }
+
+      Log.d(getClass().getSimpleName(), "Addresses is " + addresses);
+
       center =
               CameraUpdateFactory.newLatLng(new LatLng(14.553401,
                       121.0483523));
-
-
 
       CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
 
       googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
       googleMap.moveCamera(center);
       googleMap.animateCamera(zoom);
+
     } else if (spinner.getSelectedItem().toString().equals("Veritown")) {
       center =
               CameraUpdateFactory.newLatLng(new LatLng(14.5590917,
